34d92b4583e2a565e9cfe3c931557f9d6c7677f4
DG <debopamgupta3000@gmail.com>
2020-06-25 13:23:20 +0530

    Making a change

--- vercel-serverless-example/index.js
+++ vercel-serverless-example/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
+const api = require("./api/index");
+const hello = require("./api/hello");
+const json = require("./api/json");
 
 app.use(express.static("public"));
 
+app.get("/api", api);
+app.get("/api/hello", hello);
+app.get("/api/json", json);
 app.listen(process.env.PORT || 4242, () => {
   console.log("ready...");
 });
